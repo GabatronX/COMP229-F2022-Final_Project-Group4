380f71088b09148a6c2dbec740cb511a86dafcf9
GabatronX <113559626+GabatronX@users.noreply.github.com>
2022-11-11 16:13:36 -0500

    bug fix deliverable1 - GM

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 exports.home = function(req, res, next) {
-    console.log('===> Original URL: ' + req.session.url);
+    //console.log('===> Original URL: ' + req.session.url);
     res.render('index', { 
         title: 'Home',
         userName: req.user ? req.user.username : ''
